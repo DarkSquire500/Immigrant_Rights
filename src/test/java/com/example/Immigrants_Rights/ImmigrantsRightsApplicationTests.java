024b6f80fd07904ddac3016d0420e4f099ee4853
DarkSquire500 <kavasquez69@gmail.com>
2025-06-12 21:08:39 -0400

    Add files via upload

---
+++ src/test/java/com/example/Immigrants_Rights/ImmigrantsRightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Immigrants_Rights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmigrantsRightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
