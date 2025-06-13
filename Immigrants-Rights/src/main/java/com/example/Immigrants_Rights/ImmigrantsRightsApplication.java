5df275ab5a88ee5c4d70d2ed4148baa2e2f88a5f
DarkSquire500 <kavasquez69@gmail.com>
2025-06-12 21:07:58 -0400

    Add files via upload

---
+++ Immigrants-Rights/src/main/java/com/example/Immigrants_Rights/ImmigrantsRightsApplication.java
@@ -0,0 +1,14 @@
+package com.example.Immigrants_Rights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ImmigrantsRightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmigrantsRightsApplication.class, args);
+	}
+	
+}
